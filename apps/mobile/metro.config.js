6357340668ced3f61e51e1075553bd186d283b90
pntayorange <phamngoctaydl@gmail.com>
2026-05-24 13:51:38 +0000

    Install expo-font

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -9,15 +9,13 @@ const config = getDefaultConfig(projectRoot);
 // IMPORTANT: Metro needs to watch the workspace root to resolve hoisted modules
 config.watchFolders = [
   workspaceRoot,
+  ...(config.watchFolders || [])
 ];
 
 config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, "node_modules"),
   path.resolve(workspaceRoot, "node_modules"),
 ];
-
-config.resolver.unstable_enableSymlinks = true;
-config.resolver.unstable_enablePackageExports = true;
 
 config.resolver.blockList = [
   /[\\/]apps[\\/]backend[\\/]dist[\\/].*/,
